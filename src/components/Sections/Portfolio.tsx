31677f1b4b372a4ee2e0b3f7e6ffa3f86ea12b1e
David Linhardt <divad2012@web.de>
2026-05-27 18:22:30 +0200

    fix import order

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -1,8 +1,7 @@
-import {FC, memo} from 'react';
-
 import {ArrowTopRightOnSquareIcon} from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import Image from 'next/image';
+import {FC, memo} from 'react';
 
 import {portfolioItems, SectionId} from '../../data/data';
 import {PortfolioItem} from '../../data/dataDef';
